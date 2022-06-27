4d2bd1e16be76d52a7225d751ce713a2dfd6ebbb
MatthewHautala <november12erika@gmail.com>
2022-06-27 14:08:23 -0600

    categories and containers

--- src/App.js
+++ src/App.js
@@ -1,59 +1,86 @@
-import "./categories.styles.scss"
-
+import "./categories.styles.scss";
 
 const App = () => {
-
-const categories = [
-  {
-    id: 1,
-    title: 'Chassis',
-    color: "blue"
-  },
-  {
-    id: 2,
-    title: 'Engine',
-    color: "yellow"
-  },
-  {
-    id: 3,
-    title: 'Oil & Chems',
-    color: "gray"
-  },
-  {
-    id: 4,
-    title: 'Parts',
-    color: "purple"
-  },
-  {
-    id: 5,
-    title: 'Safety Gear',
-    color: "pink"
-  },
-  {
-    id: 6,
-    title: 'Turn-key Karts',
-    color: "green"
-  },
-  {
-    id: 7,
-    title: 'Classifieds',
-    color: "red"
-  }
-]
+  const categories = [
+    // {
+    //   id: 1,
+    //   title: "hats",
+    //   imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    // },
+    // {
+    //   id: 2,
+    //   title: "jackets",
+    //   imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    // },
+    // {
+    //   id: 3,
+    //   title: "sneakers",
+    //   imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    // },
+    // {
+    //   id: 4,
+    //   title: "womens",
+    //   imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    // },
+    // {
+    //   id: 5,
+    //   title: "mens",
+    //   imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    // },
+    {
+      id: 1,
+      title: 'Chassis',
+      imageUrl: 'https://kartstore-donmateo970.netlify.app/assets/van_files/chassis.jpg'
+    },
+    {
+      id: 2,
+      title: 'Engine',
+      imageUrl:  'https://kartstore-donmateo970.netlify.app/assets/van_files/engines.jpg'
+    },
+    {
+      id: 3,
+      title: 'Oil & Chems',
+      imageUrl: 'https://kartstore-donmateo970.netlify.app/assets/van_files/oils&&chemicals.jpg'
+    },
+    {
+      id: 4,
+      title: 'Parts',
+      imageUrl: 'https://kartstore-donmateo970.netlify.app/assets/van_files/parts.jpg'
+    },
+    {
+      id: 5,
+      title: 'Safety Gear',
+      imageUrl: 'https://kartstore-donmateo970.netlify.app/assets/van_files/neck.jpg'
+    },
+    {
+      id: 6,
+      title: 'Turn-key Karts',
+      imageUrl: 'https://www.theracelab.ca/wp-content/uploads/2021/12/S5205767-1024x683.jpg'
+    },
+    {
+      id: 7,
+      title: 'Classifieds',
+      imageUrl: 'https://th.bing.com/th/id/OIP.Bfr8WvP64ipPxQqxo28kfQHaE7?pid=ImgDet&rs=1'
+    }
+  ];
 
   return (
-    <div className='categories-container' >
-      {categories.map((category) => (
-        <div className='category-container'>
-        <div className="category-img" style={{backgroundColor: "blue"}}/>
-        <div className="category-body-container">
-          <h2>{category.title}</h2>
-          <p>Shop Now</p>
+    <div className="categories-container">
+      {categories.map(({ title, id, imageUrl }) => (
+        <div key={id} className="category-container">
+          <div
+            className="background-image"
+            style={{
+              backgroundImage: `url(${imageUrl})` }}
+          />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
       ))}
     </div>
   );
-}
+};
 
 export default App;
